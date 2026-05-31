2ad5cab94b104e428d80da4db24d578f47944d3a
Ragnar <ragnar.mar@hotmail.com>
2026-05-31 21:34:08 +0000

    feat: create home page with header and footer

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -17,6 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from core.views import (
+    home_screen_view
+)
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_screen_view, name='home'),
 ]
