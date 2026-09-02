c8b7aed507efaadc6f317ad90f7832fe9eb648d6
Ragnar <ragnar.mar@hotmail.com>
2026-09-02 02:38:46 +0000

    feat: added home page, and updated footer

--- src/core/views.py
+++ src/core/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from account.models import Account
 
 # Create your views here.
 
 def home_screen_view(request):
+    if request.user.is_authenticated:
+        return redirect('dashboard/dashboard') #if the user is logged in we serve the dashboard, else just a basic landing page to sign in.
 
     context = {}
 
